fe1303cf21aa97b4b9df7da007fd465d0ee69d4e
Sumosit <erlan99isaev@gmail.com>
2020-09-29 16:54:02 +0600

    1

---
+++ aspLab4/aspLab4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aspLab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
